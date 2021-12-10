a8c546d84ff666a4cc031d0a2fadb30ea590b97f
tammylam <lamtammy1515@gmail.com>
2021-12-09 23:06:29 -0600

    trying fetch api

--- app.js
+++ app.js
@@ -4,7 +4,20 @@ window.addEventListener('load', () => {
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
-            console.log(position)
+            long = position.coords.longitude
+            lat = position.coords.latitude;
+
+            const api = `http://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`
+
+            fetch(api)
+            .then(resp =>{
+                return resp.json();
+            })
+            .then(data => {
+                console.log(data)
+            })
         })
     }
 })
+
+//http://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/37.8267,-122.4233
